c0975987b99f9a3e8078fde6e8685188fa948e7b
YauhenKoltashau <yauhen.koltashau.1985@gmail.com>
2022-03-09 12:45:53 +0300

    compare var and let, mutate const Array, default function,use and write arrow function

--- Compare Scopes of the var and let Keywords.js
+++ Compare Scopes of the var and let Keywords.js
@@ -1,6 +1,32 @@
-var numArray = [];
+var printNumTwo;
 for (var i = 0; i < 3; i++) {
-  numArray.push(i);
+  if (i === 2) {
+    printNumTwo = function() {
+      return i;
+    };
+  }
 }
-console.log(numArray);
-console.log(i);
+console.log(printNumTwo());
+console.log(i);//global
+
+let printNum;
+for (let j = 0; j < 3; j++) {
+  if (j === 2) {
+    printNum = function() {
+      return j;// local
+    };
+  }
+}
+console.log(printNum());
+
+function checkScope() {
+    var i = 'function scope';
+    if (true) {
+      let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+
+  checkScope()
